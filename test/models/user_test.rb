4e4252791a4db6d76aa936411f2d6afca1e9275f
chou <a2224wz@aiit.ac.jp>
2022-05-06 21:34:16 +0900

    user.rb user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,10 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
-    def setup
-     @user = User.new(name: "Example User", email: "user@example.com")
-    end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
    test "should be valid" do
      assert @user.valid?
