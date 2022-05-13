a722162c11bba610ddca67953aeb9af616f23155
chou <a2224wz@aiit.ac.jp>
2022-05-13 21:40:30 +0900

    debug info

--- db/migrate/20220506085532_create_users.rb
+++ db/migrate/20220506085532_create_users.rb
@@ -1,5 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+  def change
     add_index :users, :email, unique: true
   end
 end
