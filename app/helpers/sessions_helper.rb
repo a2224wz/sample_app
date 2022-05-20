f52c8b06de776ff4ef543ac67c8826f3291547e1
chou <a2224wz@aiit.ac.jp>
2022-05-20 21:06:05 +0900

    correct_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,4 +21,29 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークン（cookie）に対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
 end
