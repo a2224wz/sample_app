e1efebf0a1e9b6a9202d182143fd59e026a9bd72
chou <a2224wz@aiit.ac.jp>
2022-06-03 23:00:23 +0900

    Micropostテスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_134739) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_135546) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_134739) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
